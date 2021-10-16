511f91d180a7447b95e9e792778d304445ff4713
gabrielrtl <56652728+gabrielrtl@users.noreply.github.com>
2021-10-16 20:57:38 -0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	#Definições para identificar recursos relacionado ao banco
-	define ('HOSTNAME', 'localhost');
-	define ('USERNAME', 'user');
-	define ('PASSWORD', null);
-	define ('DATABASE', 'sisweb');
-	define ('CHARSET', 'utf8');
-?>
